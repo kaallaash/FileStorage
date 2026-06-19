75322e095440917db662c377ee24128900aeac0e
Andrey <kaallaash@gmail.com>
2026-06-19 10:35:23 +0300

    add HeroWars files

---
+++ HeroWars/HW_AIO.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         HW AIO
+// @namespace    HW AIO
+// @version      12.5.0
+// @description  Помогает найти лучшую защиту на ТС
+// @author		 kaallaash
+// @match        https://www.hero-wars.com/*
+// @match        https://apps-*.fbsbx.com/*
+// @downloadURL  https://kaallaash.github.io/FileStorage/HeroWars/HW_AIO.user.js
+// @updateURL    https://kaallaash.github.io/FileStorage/HeroWars/HW_AIO.meta.js
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @connect      14283401.fls.doubleclick.net
+// @connect      api.github.com
+// @connect      kaallaash.github.io
+// @connect      t1.kakaocdn.net
+// @connect      www.hero-wars.com
+// @connect      www.recaptcha.net
+// @connect      hw-aio-subs-info.somee.com
+// @connect      heroes-vk.nextersglobal.com
+// ==/UserScript==
